407ca18537f14cdeef3917984ae4b4bfa707b583
Аюр Цыбенов <Krovvar14@yandex.ru>
2022-10-31 14:21:11 +0800

    Добавлен debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -141,3 +141,19 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+if DEBUG:
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+    INTERNAL_IPS = [
+        '127.0.0.1',
+    ]
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
